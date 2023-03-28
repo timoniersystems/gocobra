e3586c42deafa39029eddf9737e356456fa3b9f0
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-28 20:04:01 +0000

    install and run slsa-verifier

--- scripts/run_slsa_verifier.sh
+++ scripts/run_slsa_verifier.sh
@@ -5,10 +5,13 @@ SOURCE_TAG=2.0.4
 ARTIFACT_FILE=gocobra-linux-amd64-slsa3
 INTOTO_ATTESTATION=gocobra-linux-amd64-slsa3.intoto.jsonl
 
+rm -rf $ARTIFACT_FILE
+rm -rf $INTOTO_ATTESTATION
+
 wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${ARTIFACT_FILE}
 wget https://${SOURCE_URI}/releases/download/${SOURCE_TAG}/${INTOTO_ATTESTATION}
 
-~/go/bin/slsa-verifier verify-artifact $ARTIFACT_FILE \
+slsa-verifier verify-artifact $ARTIFACT_FILE \
   --provenance-path $INTOTO_ATTESTATION \
   --source-uri $SOURCE_URI \
   --source-tag $SOURCE_TAG \
