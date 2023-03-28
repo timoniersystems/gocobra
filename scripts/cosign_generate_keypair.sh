2c3d90a760f18db0bcd2b01d793c87488d8db9b6
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-28 20:41:12 +0000

    also generate base64 value for cosign key to be saved as GHA secret

--- scripts/cosign_generate_keypair.sh
+++ scripts/cosign_generate_keypair.sh
@@ -5,3 +5,4 @@ PRIVATE_KEY_PASS=$(cat ~/.k)
 echo -n $PRIVATE_KEY_PASS | cosign generate-key-pair
 mkdir -p ~/.cosign
 mv cosign.* ~/.cosign
+cat ~/.cosign/cosign.key | base64 -w 0 > ~/.cosign/cosign.key.base64
