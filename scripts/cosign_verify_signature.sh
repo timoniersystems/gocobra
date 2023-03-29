4fb4371725db265c71d46d529ec10dba401bafef
Grig Gheorghiu <grig@gheorghiu.net>
2023-03-29 00:51:22 +0000

    use IMAGE_NAME and IMAGE_TAG

--- scripts/cosign_verify_signature.sh
+++ scripts/cosign_verify_signature.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-COSIGN_PUBLIC_KEY=$HOME/.cosign/cosign.pub
-IMAGE=ghcr.io/timoniersystems/gocobra:2.0.8
+IMAGE_TAG=2.2.7
 
-cosign verify --key $COSIGN_PUBLIC_KEY "$IMAGE"
+COSIGN_PUBLIC_KEY=$HOME/.cosign/cosign.pub
+IMAGE_NAME=ghcr.io/timoniersystems/gocobra
+
+cosign verify --key $COSIGN_PUBLIC_KEY ${IMAGE_NAME}:${IMAGE_TAG}
 
 ### Triangulation
 
 # show signature artifact
-cosign triangulate $IMAGE
+cosign triangulate ${IMAGE_NAME}:${IMAGE_TAG}
 
 # show signature manifest
-crane manifest $(cosign triangulate $IMAGE) | jq .
+crane manifest $(cosign triangulate ${IMAGE_NAME}:${IMAGE_TAG}) | jq .
